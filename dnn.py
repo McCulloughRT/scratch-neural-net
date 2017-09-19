70e279365b487c9b9d0ac0fbd1ab3192612572fa
Ryan McCullough <McCulloughRT@gmail.com>
2017-09-19 10:39:24 -0700

    Update dnn.py

--- dnn.py
+++ dnn.py
@@ -332,7 +332,7 @@ def L_layer_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 30
     
     Arguments:
     X -- data, numpy array of shape (number of examples, num_px * num_px * 3)
-    Y -- true "label" vector (containing 0 if cat, 1 if non-cat), of shape (1, number of examples)
+    Y -- true binary "label" vector (containing 0 if negative, 1 if positive), of shape (1, number of examples)
     layers_dims -- list containing the input size and each layer size, of length (number of layers + 1).
     learning_rate -- learning rate of the gradient descent update rule
     num_iterations -- number of iterations of the optimization loop
